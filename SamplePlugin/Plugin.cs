7382a8ed3623bd7013839f560c40a52f25bb754a
meli <57847713+ff-meli@users.noreply.github.com>
2020-04-28 13:21:51 -0700

    Initial simple sample plugin and associated UI testbed

---
+++ SamplePlugin/Plugin.cs
@@ -0,0 +1,64 @@
+﻿using Dalamud.Game.Command;
+using Dalamud.Plugin;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace SamplePlugin
+{
+    public class Plugin : IDalamudPlugin
+    {
+        public string Name => "Sample Plugin";
+
+        private const string commandName = "/pmycommand";
+
+        private DalamudPluginInterface pi;
+        private Configuration configuration;
+        private PluginUI ui;
+
+        public void Initialize(DalamudPluginInterface pluginInterface)
+        {
+            this.pi = pluginInterface;
+            
+            this.configuration = this.pi.GetPluginConfig() as Configuration ?? new Configuration();
+            this.configuration.Initialize(this.pi);
+
+            // you might normally want to embed resources and load them from the manifest stream
+            var imagePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"goat.png");
+            var goatImage = this.pi.UiBuilder.LoadImage(imagePath);
+            this.ui = new PluginUI(this.configuration, goatImage);
+
+            this.pi.CommandManager.AddHandler(commandName, new CommandInfo(OnCommand)
+            {
+                HelpMessage = "A useful message to display in /xlhelp"
+            });
+
+            this.pi.UiBuilder.OnBuildUi += DrawUI;
+            this.pi.UiBuilder.OnOpenConfigUi += (sender, args) => DrawConfigUI();
+        }
+
+        public void Dispose()
+        {
+            this.ui.Dispose();
+
+            this.pi.CommandManager.RemoveHandler(commandName);
+            this.pi.Dispose();
+        }
+
+        private void OnCommand(string command, string args)
+        {
+            // in response to the slash command, just display our main ui
+            this.ui.Visible = true;
+        }
+
+        private void DrawUI()
+        {
+            this.ui.Draw();
+        }
+
+        private void DrawConfigUI()
+        {
+            this.ui.SettingsVisible = true;
+        }
+    }
+}
---
+++ SamplePlugin/PluginUI.cs
@@ -0,0 +1,106 @@
+﻿using ImGuiNET;
+using System;
+using System.Numerics;
+
+namespace SamplePlugin
+{
+    // It is good to have this be disposable in general, in case you ever need it
+    // to do any cleanup
+    class PluginUI : IDisposable
+    {
+        private Configuration configuration;
+
+        private ImGuiScene.TextureWrap goatImage;
+
+        // this extra bool exists for ImGui, since you can't ref a property
+        private bool visible = false;
+        public bool Visible
+        {
+            get { return this.visible; }
+            set { this.visible = value; }
+        }
+
+        private bool settingsVisible = false;
+        public bool SettingsVisible
+        {
+            get { return this.settingsVisible; }
+            set { this.settingsVisible = value; }
+        }
+
+        // passing in the image here just for simplicity
+        public PluginUI(Configuration configuration, ImGuiScene.TextureWrap goatImage)
+        {
+            this.configuration = configuration;
+            this.goatImage = goatImage;
+        }
+
+        public void Dispose()
+        {
+            this.goatImage.Dispose();
+        }
+
+        public void Draw()
+        {
+            // This is our only draw handler attached to UIBuilder, so it needs to be
+            // able to draw any windows we might have open.
+            // Each method checks its own visibility/state to ensure it only draws when
+            // it actually makes sense.
+            // There are other ways to do this, but it is generally best to keep the number of
+            // draw delegates as low as possible.
+
+            DrawMainWindow();
+            DrawSettingsWindow();
+        }
+
+        public void DrawMainWindow()
+        {
+            if (!Visible)
+            {
+                return;
+            }
+
+            ImGui.SetNextWindowSize(new Vector2(375, 330), ImGuiCond.FirstUseEver);
+            ImGui.SetNextWindowSizeConstraints(new Vector2(375, 330), new Vector2(float.MaxValue, float.MaxValue));
+            if (ImGui.Begin("My Amazing Window", ref this.visible, ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoScrollWithMouse))
+            {
+                ImGui.Text($"The random config bool is {this.configuration.SomePropertyToBeSavedAndWithADefault}");
+
+                if (ImGui.Button("Show Settings"))
+                {
+                    SettingsVisible = true;
+                }
+
+                ImGui.Spacing();
+
+                ImGui.Text("Have a goat:");
+                ImGui.Indent(55);
+                ImGui.Image(this.goatImage.ImGuiHandle, new Vector2(this.goatImage.Width, this.goatImage.Height));
+                ImGui.Unindent(55);
+            }
+            ImGui.End();
+        }
+
+        public void DrawSettingsWindow()
+        {
+            if (!SettingsVisible)
+            {
+                return;
+            }
+
+            ImGui.SetNextWindowSize(new Vector2(232, 75), ImGuiCond.Always);
+            if (ImGui.Begin("A Wonderful Configuration Window", ref this.settingsVisible,
+                ImGuiWindowFlags.NoResize | ImGuiWindowFlags.NoCollapse | ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoScrollWithMouse))
+            {
+                // can't ref a property, so use a local copy
+                var configValue = this.configuration.SomePropertyToBeSavedAndWithADefault;
+                if (ImGui.Checkbox("Random Config Bool", ref configValue))
+                {
+                    this.configuration.SomePropertyToBeSavedAndWithADefault = configValue;
+                    // can save immediately on change, if you don't want to provide a "Save and Close" button
+                    this.configuration.Save();
+                }
+            }
+            ImGui.End();
+        }
+    }
+}
