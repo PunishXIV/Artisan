052ebe49f7a5eae1650e9abe11c1204c32951761
Taurenkey <github@taurenkey.co.uk>
2022-08-30 19:41:03 +0100

    Adds delay check + hopefully mitigate FPS issues

--- Artisan/Configuration.cs
+++ Artisan/Configuration.cs
@@ -43,6 +43,8 @@ namespace Artisan
         public bool UseMacroMode { get; set; }
         public Macro? SetMacro { get; set; }
 
+        public int AutoDelay { get; set; } = 0;
+
 
         public uint Food = 0;
         public uint Potion = 0;
