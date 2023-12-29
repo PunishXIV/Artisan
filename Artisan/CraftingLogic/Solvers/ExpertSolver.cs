09c6d89e5bc45f85eff55bea3f88f5a426fbf003
Taurenkey <github@taurenkey.co.uk>
2023-12-29 13:35:15 +0000

    Fix bad merge

--- Artisan/CraftingLogic/Solvers/ExpertSolver.cs
+++ Artisan/CraftingLogic/Solvers/ExpertSolver.cs
@@ -73,7 +73,7 @@ public class ExpertSolver : Solver
         }
 
         if (step.MuscleMemoryLeft > 0) // mume still active - means we have very little progress and want more progress asap
-            return (SafeCraftAction(craft, step, SolveOpenerMuMe(cfg, craft, step)), "mume");
+            return new(SolveOpenerMuMe(cfg, craft, step), "mume");
 
         // see what we need to finish the craft
         var remainingProgress = craft.CraftProgress - step.Progress;
