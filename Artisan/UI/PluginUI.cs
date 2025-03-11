a0ce3d04a33510b8ca1d289229990b2d0744c9ac
wolfcomp <4028289+wolfcomp@users.noreply.github.com>
2025-03-11 11:49:27 +0100

    Add red text style

--- Artisan/UI/PluginUI.cs
+++ Artisan/UI/PluginUI.cs
@@ -87,8 +87,10 @@ namespace Artisan.UI
         {
             if (DalamudInfo.IsOnStaging())
             {
+                ImGui.PushStyleColor(ImGuiCol.Text, ImGuiColors.DalamudRed);
                 ImGui.Text("Artisan is not designed to work on non-release versions of Dalamud. Please type /xlbranch, click 'release' and then 'Pick & Restart'.");
                 ImGui.Text("Any bugs created will not be supported until it is recreated on the release branch.");
+                ImGui.PopStyleColor();
             }
 
             var region = ImGui.GetContentRegionAvail();
