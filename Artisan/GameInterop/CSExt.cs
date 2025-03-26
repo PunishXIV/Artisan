2d5334485e41da701cc094c9dff2fffb8ebafa6b
Taurenkey <github@taurenkey.co.uk>
2025-03-26 19:04:19 +0000

    Fix CSExt

--- Artisan/GameInterop/CSExt.cs
+++ Artisan/GameInterop/CSExt.cs
@@ -93,7 +93,7 @@ public unsafe struct RecipeNoteRecipeData
 
     [FieldOffset(0x000)] public RecipeNoteRecipeEntry* Recipes; // note: can be null
     [FieldOffset(0x008)] public int RecipesCount;
-    [FieldOffset(0x428)] public ushort SelectedIndex;
+    [FieldOffset(0x438)] public ushort SelectedIndex;
 
     public RecipeNoteRecipeEntry* FindRecipeById(uint id)
     {
