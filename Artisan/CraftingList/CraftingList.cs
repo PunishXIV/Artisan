8f2d6855298f15e5c4ae984c71e57067f5bfbb01
Taurenkey <github@taurenkey.co.uk>
2026-01-12 17:55:36 +0000

    List consumable quick synth bs

--- Artisan/CraftingList/CraftingList.cs
+++ Artisan/CraftingList/CraftingList.cs
@@ -380,10 +380,10 @@ namespace Artisan.CraftingLists
                 return;
             }
 
-            bool needFood = config != default && ConsumableChecker.HasItem(config.RequiredFood, config.RequiredFoodHQ) && !ConsumableChecker.IsFooded(config);
-            bool needPot = config != default && ConsumableChecker.HasItem(config.RequiredPotion, config.RequiredPotionHQ) && !ConsumableChecker.IsPotted(config);
-            bool needManual = config != default && ConsumableChecker.HasItem(config.RequiredManual, false) && !ConsumableChecker.IsManualled(config);
-            bool needSquadronManual = config != default && ConsumableChecker.HasItem(config.RequiredSquadronManual, false) && !ConsumableChecker.IsSquadronManualled(config);
+            bool needFood = type is PreCrafting.CraftType.Quick && !P.Config.UseConsumablesQuickSynth ? false : config != default && ConsumableChecker.HasItem(config.RequiredFood, config.RequiredFoodHQ) && !ConsumableChecker.IsFooded(config);
+            bool needPot = type is PreCrafting.CraftType.Quick && !P.Config.UseConsumablesQuickSynth ? false : config != default && ConsumableChecker.HasItem(config.RequiredPotion, config.RequiredPotionHQ) && !ConsumableChecker.IsPotted(config);
+            bool needManual = type is PreCrafting.CraftType.Quick && !P.Config.UseConsumablesQuickSynth ? false : config != default && ConsumableChecker.HasItem(config.RequiredManual, false) && !ConsumableChecker.IsManualled(config);
+            bool needSquadronManual = type is PreCrafting.CraftType.Quick && !P.Config.UseConsumablesQuickSynth ? false : config != default && ConsumableChecker.HasItem(config.RequiredSquadronManual, false) && !ConsumableChecker.IsSquadronManualled(config);
 
             if (needFood || needPot || needManual || needSquadronManual)
             {
