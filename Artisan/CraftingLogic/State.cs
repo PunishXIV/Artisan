edc677f977a84ccb153758ab7363ad4bc7a0019d
pksage <pk.sage@gmail.com>
2026-03-21 19:18:42 -0700

    Expert solver: Add condition probabilities per ConditionsFlag

--- Artisan/CraftingLogic/State.cs
+++ Artisan/CraftingLogic/State.cs
@@ -1,5 +1,6 @@
 ﻿using Artisan.CraftingLogic.CraftData;
 using Artisan.RawInformation.Character;
+using System.Collections.Generic;
 using System.Text;
 
 namespace Artisan.CraftingLogic;
@@ -53,6 +54,33 @@ public record class CraftState
     public static float[] EWRelicT1CraftConditionProbabilities() => [1, 0.03f, 0, 0, 0.12f, 0.12f, 0.12f, 0, 0, 0.12f];
     public static float[] EWRelicT2CraftConditionProbabilities() => [1, 0.04f, 0, 0, 0, 0.15f, 0.12f, 0.12f, 0.15f, 0.12f];
     public static float[] EW5StarCraftConditionProbabilities() => [1, 0.04f, 0, 0, 0.12f, 0.12f, 0.10f, 0.10f, 0.12f, 0.12f];
+
+    // these probabilities were gathered with at least 10,000 steps collected per condition set
+    //@formatter:off
+    public static Dictionary<ushort, float[]> ExpertConditionOdds = new Dictionary<ushort, float[]>()
+    {
+        //       Normal, Good,    Excell, Poor,   Center, Sturdy, Pliant, Mallea, Primed, GoodOm, Robust
+        { 0,    [1f,     0,       0,      0,      0,      0,      0,      0,      0,      0,      0     ] },
+        { 115,  [0.47f,  0.12f,   0,      0,      0.15f,  0.15f,  0.11f,  0,      0,      0,      0     ] },
+        { 483,  [0.38f,  0.12f,   0,      0,      0,      0.15f,  0.12f,  0.12f,  0.11f,  0,      0     ] },
+        { 499,  [0.32f,  0.12f,   0,      0,      0.14f,  0.10f,  0.10f,  0.10f,  0.14f,  0,      0     ] },
+        { 435,  [0.45f,  0.11f,   0,      0,      0.11f,  0.11f,  0,      0.11f,  0.11f,  0,      0     ] },
+        { 995,  [0.34f,  0.04f,   0,      0,      0,      0.14f,  0.11f,  0.11f,  0.14f,  0.12f,  0     ] },
+        { 627,  [0.43f,  0.04f,   0,      0,      0.15f,  0.15f,  0.12f,  0,      0,      0.11f,  0     ] },
+        { 1011, [0.22f,  0.12f,   0,      0,      0.12f,  0.12f,  0.10f,  0.10f,  0.12f,  0.10f,  0     ] },
+        { 19,   [0.44f,  0.12f,   0,      0,      0.44f,  0,      0,      0,      0,      0,      0     ] },
+        { 67,   [0.44f,  0.12f,   0,      0,      0,      0,      0.44f,  0,      0,      0,      0     ] },
+        { 35,   [0.44f,  0.12f,   0,      0,      0,      0.44f,  0,      0,      0,      0,      0     ] },
+        { 515,  [0.68f,  0.02f,   0,      0,      0,      0,      0,      0,      0,      0.30f,  0     ] },
+        { 259,  [0.44f,  0.12f,   0,      0,      0,      0,      0,      0,      0.44f,  0,      0     ] },
+        { 131,  [0.44f,  0.12f,   0,      0,      0,      0,      0,      0.44f,  0,      0,      0     ] },
+        { 1363, [0.45f,  0.12f,   0,      0,      0.13f,  0,      0.10f,  0,      0.12f,  0,      0.08f ] },
+        { 1491, [0.26f,  0.16f,   0,      0,      0.12f,  0,      0.11f,  0.15f,  0.11f,  0,      0.09f ] },
+        { 243,  [0.29f,  0.12f,   0,      0,      0.19f,  0.12f,  0.15f,  0.13f,  0,      0,      0     ] },
+        { 1459, [0.32f,  0.12f,   0,      0,      0.15f,  0.10f,  0,      0.12f,  0.14f,  0,      0.05f ] },
+        { 1523, [0.20f,  0.12f,   0,      0,      0.15f,  0.08f,  0.15f,  0.10f,  0.10f,  0,      0.10f ] },
+    };
+    //@formatter:on
 }
 
 public record class StepState
