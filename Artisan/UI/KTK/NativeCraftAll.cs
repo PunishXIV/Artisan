5dc481b2b6ba50bb43e4f29a876b48292c43c6e6
Kage <95875972+Kagekazu@users.noreply.github.com>
2026-07-03 12:43:33 +0200

    revert button

--- Artisan/UI/KTK/NativeCraftAll.cs
+++ Artisan/UI/KTK/NativeCraftAll.cs
@@ -13,7 +13,7 @@ namespace Artisan.UI.KTK
     {
         AddonController? regularRecipeNoteController;
         AddonController? moonRecipeNoteController;
-        TextButtonNode? SynthesizeAll;
+        TextButtonNodeSynth? SynthesizeAll;
         NumericInputNode? SynthesizeXCounter;
         int val = 0;
         int SynthesizeableCount = 0;
